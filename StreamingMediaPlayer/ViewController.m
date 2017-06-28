c978b5fd0cac0a3ebf981b70515a6e0d3ecd686c
qhzc-iMac-02 <qhzc-imac-02@qhzc-iMac-02deiMac.local>
2017-06-28 13:47:23 +0800

    新建工程

---
+++ StreamingMediaPlayer/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  StreamingMediaPlayer
+//
+//  Created by qhzc-iMac-02 on 2017/6/28.
+//  Copyright © 2017年 Yxl. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
